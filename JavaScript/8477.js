97750d5b0043f1823327c4bdd46e95bddf83589f
siwon99 <126474541+siwon99@users.noreply.github.com>
2024-03-18 16:13:30 +0900

    8477.js Upload

---
+++ JavaScript/8477.js
@@ -0,0 +1,22 @@
+let num1 = [30, 60, 90] // P
+let num2 = [20, 20, 30] // F
+
+function solution1 (num1) {
+    if(num1[0] + num1[1] + num1[2] == 180) {
+        console.log('P')
+    }
+    else {
+        console.log('F')
+    }
+}
+solution1 (num1)
+
+function solution1 (num2) {
+    if(num2[0] + num2[1] + num2[2] == 180) {
+        console.log('P')
+    }
+    else {
+        console.log('F')
+    }
+}
+solution1 (num2)
