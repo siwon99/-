8ba2e92b8edd05bb3e2b14101cfb6fb1fcb599d8
siwon99 <126474541+siwon99@users.noreply.github.com>
2024-03-24 00:19:13 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+    var a = 0
+    
+    for (let i = 0; i < arr.length; i ++) {
+           a += arr[i] 
+           var answer = a / arr.length;
+        }
+    return answer;
+}
