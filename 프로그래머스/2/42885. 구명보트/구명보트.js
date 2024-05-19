45e6c2f813d5fd0c072f379b05116e62a84bb921
Kim Si Won <126474541+siwon99@users.noreply.github.com>
2024-05-19 21:20:12 +0900

    [level 2] Title: 구명보트, Time: 14.78 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,16 @@
+function solution(people, limit) {
+    let count = 0;
+    people.sort((a, b) => a - b);
+    
+    while(people.length !== 0) {
+        if(people[0] + people[people.length - 1] <= limit) {
+            count++;
+            people.shift();
+            people.pop();
+        } else {
+            count++;
+            people.pop()
+        }
+    }
+    return count;
+}
