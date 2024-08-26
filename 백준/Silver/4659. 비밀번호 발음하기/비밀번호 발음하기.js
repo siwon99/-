609f949e82b2271fa6915bfc089817b4208dd689
Kim Si Won <126474541+siwon99@users.noreply.github.com>
2024-08-26 15:34:40 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 92 ms, Memory: 9436 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.js
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.js
@@ -1,26 +1,40 @@
 const fs = require('fs');
 const words = fs.readFileSync("/dev/stdin").toString().trim().split('\n').map(v => v.trim());
-words.pop();
+words.pop(); //end 삭제
 
-function solution(word) {
-  const m = /[aeiou]/;
-  const threeM = /[aeiou]{3}/; // 연속된 모음
-  const threeEct = /[^aeiou]{3}/; // 연속된 자음
-  const double = /(.)\1/; // 같은 글자 두번 반복
+function solution(words) {
+  for(let i = 0; i < words.length; i++) {
+    let vcount = 0; // 모음 연속 체크
+    let lcount = 0; // 자음 연속 체크
+    let includeVcheck = false; // 모음 포함 여부
+    let countCheck = true; // 3개 연속 체크
+    let eoCheck = true; // ee와 oo만 허용
 
-  if (!m.test(word)) {
-    console.log(`<${word}> is not acceptable.`);
-    return;
+    let v = ['a', 'e', 'i', 'o', 'u'];
+    for(let j = 0; j < words[i].length; j++) {
+      if(v.includes(words[i][j])) {
+        includeVcheck = true;
+        vcount++;
+        lcount = 0;
+      } else {
+        vcount = 0;
+        lcount++;
+      }
+
+      if(vcount === 3 || lcount === 3) {
+        countCheck = false;
+      }
+
+      if(words[i][j] === words[i][j + 1] && words[i][j] !== 'e' && words[i][j] !== 'o') {
+          eoCheck = false;
+      }
+    }
+    if(includeVcheck && countCheck && eoCheck) {
+      console.log(`<${words[i]}> is acceptable.`);
+    } else {
+      console.log(`<${words[i]}> is not acceptable.`);
+    }
   }
-  if (threeM.test(word) || threeEct.test(word)) {
-    console.log(`<${word}> is not acceptable.`);
-    return;
-  }
-  if (double.test(word) && !/ee|oo/.test(word)) {
-    console.log(`<${word}> is not acceptable.`);
-    return;
-  }
-  console.log(`<${word}> is acceptable.`);
 }
 
-words.forEach(solution);
+solution(words);
