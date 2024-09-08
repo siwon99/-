7fa069ca3255d32177807c36bf8ac0167395076e
Kim Si Won <126474541+siwon99@users.noreply.github.com>
2024-09-08 17:16:54 +0900

    [Silver I] Title: 스티커, Time: 360 ms, Memory: 51436 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.js
+++ 백준/Silver/9465. 스티커/스티커.js
@@ -1,22 +1,23 @@
 const input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n").map((e) => e.split(" ").map(Number));
 
- const T = input[0][0];
- for (let j = 0; j < T; j++) {
-   const n = input[3 * j + 1][0];
-   let array = new Array(2);
-   let dp = Array.from(Array(2), () => new Array(n + 1).fill(0));
-     
-   array[0] = input[3 * j + 2];
-   array[1] = input[3 * j + 3];
-     
-   dp[0][0] = array[0][0];
-   dp[1][0] = array[1][0];
-   dp[0][1] = dp[1][0] + array[0][1];
-   dp[1][1] = dp[0][0] + array[1][1];
-     
-   for (let i = 2; i < n; i++) {
-     dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + array[0][i];
-     dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + array[1][i];
-   }
-   console.log(Math.max(dp[0][n - 1], dp[1][n - 1]));
- }
+const T = input[0][0];
+for (let i = 0; i < T; i++) {
+  const n = input[3 * i + 1][0];
+  let array = new Array(2);
+  let dp = Array.from(Array(2), () => new Array(n + 1).fill(0));
+
+  array[0] = input[3 * i + 2];
+  array[1] = input[3 * i + 3];
+
+  dp[0][0] = array[0][0];
+  dp[1][0] = array[1][0];
+  dp[0][1] = dp[1][0] + array[0][1];
+  dp[1][1] = dp[0][0] + array[1][1];
+
+  for (let j = 2; j < n; j++) {
+    dp[0][j] = Math.max(dp[1][j - 1], dp[1][j - 2]) + array[0][j];
+    dp[1][j] = Math.max(dp[0][j - 1], dp[0][j - 2]) + array[1][j];
+  }
+
+  console.log(Math.max(dp[0][n - 1], dp[1][n - 1]));
+}
